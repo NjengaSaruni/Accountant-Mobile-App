a91546848263c8cb139ceb04e0acd53f21dce752
Peter Njenga <peternjengask@gmail.com>
2019-02-25 13:01:08 +0300

    Handle click events

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,51 @@ class MyApp extends StatelessWidget {
 
 }
 
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavorite,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
+  }
+}
+
 class MyStatelessWidget extends StatelessWidget {
   MyStatelessWidget({Key key}) : super(key: key);
 
@@ -54,11 +99,7 @@ class MyStatelessWidget extends StatelessWidget {
             ),
           ),
           /*3*/
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41'),
+          FavoriteWidget(),
         ],
       ),
     );
@@ -124,7 +165,7 @@ class MyStatelessWidget extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
+      body: ListView(
         children: <Widget>[
           Image.asset(
             'images/lake.jpg',
@@ -134,7 +175,13 @@ class MyStatelessWidget extends StatelessWidget {
           ),
           titleSection,
           buttonSection,
-          textSection
+          textSection,
+          Image.asset(
+            'images/lake.jpg',
+            width: 600,
+            height: 240,
+            fit: BoxFit.cover,
+          ),
         ],
       ),
     );
