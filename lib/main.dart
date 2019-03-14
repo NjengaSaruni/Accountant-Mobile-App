481993757cb93bd1550fc8d3b333039cf74eb02b
Peter Njenga <peternjengask@gmail.com>
2019-03-14 23:08:26 +0300

    We are back

--- lib/main.dart
+++ lib/main.dart
@@ -165,7 +165,7 @@ class MyStatelessWidget extends StatelessWidget {
     Widget textSection = Container(
       padding: const EdgeInsets.all(32),
       child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
+        'Lake Victoria lies at the foot of the Blüemlisalp in the Bernese '
             'Alps. Situated 1,578 meters above sea level, it is one of the '
             'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
             'half-hour walk through pastures and pine forest, leads you to the '
